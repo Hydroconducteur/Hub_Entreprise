bc575ba45b88e520d367d7bba9ba2088b6338e0e
Hydroconducteur <166796408+Hydroconducteur@users.noreply.github.com>
2026-05-27 15:07:10 +0200

    Update app.py

--- app.py
+++ app.py
@@ -361,13 +361,15 @@ def initialiser_structure_base():
     try: cursor.execute("ALTER TABLE utilisateurs ADD COLUMN code_secret TEXT DEFAULT '1234'")
     except sqlite3.OperationalError: pass
     
-    cursor.execute("SELECT prenom, code_secret FROM utilisateurs WHERE prenom = 'Christophe'")
+cursor.execute("SELECT prenom, code_secret FROM utilisateurs WHERE prenom = 'Christophe'")
     admin_row = cursor.fetchone()
     
+    mdp_admin = st.secrets["ADMIN_PASS"]
+    
     if not admin_row:
-        cursor.execute("INSERT OR REPLACE INTO utilisateurs (prenom, code_secret) VALUES ('Christophe', 'Admin45')")
+        cursor.execute("INSERT OR REPLACE INTO utilisateurs (prenom, code_secret) VALUES ('Christophe', ?)", (mdp_admin,))
     elif admin_row[1] == '1234' or admin_row[1] is None or admin_row[1] == '':
-        cursor.execute("UPDATE utilisateurs SET code_secret = 'Admin45' WHERE prenom = 'Christophe'")
+        cursor.execute("UPDATE utilisateurs SET code_secret = ? WHERE prenom = 'Christophe'", (mdp_admin,))
         
     conn.commit()
 
