e143d975d9373318d8495aed3443e6301a62b6c0
Hydroconducteur <166796408+Hydroconducteur@users.noreply.github.com>
2026-06-04 10:04:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1332,7 +1332,8 @@ elif page == "📦 Demande Fournisseur":
             st.subheader("🏢 Expéditeur")
             # Attention : il y a deux espaces à la fin de chaque ligne ci-dessous !
             st.markdown("""
-            **Quincaillerie MAUPU** 3 rue du Mail Est  
+            **Quincaillerie MAUPU** 
+            3 rue du Mail Est  
             45170 Neuville-Aux-Bois  
             02 38 91 00 15  
             quincaillerie.maupu@cegetel.net
